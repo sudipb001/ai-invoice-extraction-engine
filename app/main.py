cd9a3c38c52c1a3d1d56fdb27e0bb802a1a1d026
Sudip Bhattacharyya <sudipb001@gmail.com>
2026-04-23 15:53:40 +0530

    Module 02

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from pathlib import Path
+import uuid
 
 app = FastAPI(title="AI Invoice Extraction Engine")
+
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
 
 @app.get("/")
 def home():
     return {"message": "API Running Successfully"}
+
+@app.post("/upload")
+async def upload_invoice(file: UploadFile = File(...)):
+    
+    if not file.filename.lower().endswith(".pdf"):
+        return {"error": "Only PDF files allowed"}
+
+    unique_name = f"{uuid.uuid4()}_{file.filename}"
+    save_path = UPLOAD_DIR / unique_name
+
+    content = await file.read()
+
+    with open(save_path, "wb") as f:
+        f.write(content)
+
+    return {
+        "message": "Upload successful",
+        "filename": unique_name
+    }
